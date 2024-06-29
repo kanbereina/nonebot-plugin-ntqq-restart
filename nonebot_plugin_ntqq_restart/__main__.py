5197451c0300cdb46be2e687263b00d2c7b971e8
鹿友-Reina <121220148+kanbereina@users.noreply.github.com>
2024-06-29 23:23:37 +0800

    更新v2.0版本

--- nonebot_plugin_ntqq_restart/__main__.py
+++ nonebot_plugin_ntqq_restart/__main__.py
@@ -1,45 +1,49 @@
 from datetime import datetime, timedelta
 
-import nonebot
+from nonebot import get_plugin_config, get_driver
 from nonebot_plugin_apscheduler import scheduler
 
 from .config import Config
-from .utils import NTQQ, run_qq_main, restart_qq_main
+from .utils import (
+    NTQQ,
+    ntqq_path_checker,
+    run_nqq_main, restart_nqqq_main
+)
 
 
-# 检查相关值
-NTQQ.check_path_value()
-NTQQ.check_file_exists()
+# 检查NTQQ路径
+ntqq_path_checker()
 
 
-Driver = nonebot.get_driver()
-PluginConfig = nonebot.get_plugin_config(Config)
+Driver = get_driver()
+PluginConfig = get_plugin_config(Config)
 
 
 @Driver.on_startup
 async def start_qq_mian():
-    if PluginConfig.enable_login_when_nbrun:  # 允许运行nb的时候进行登录
-        scheduler.add_job(  # 防一直处于on_startup状态, 无法最小化窗口
-            run_qq_main, "date",
-            next_run_time=datetime.now() + timedelta(seconds=1)
-        )
+    scheduler.add_job(  # 防一直处于on_startup状态, 无法最小化窗口
+        run_nqq_main, "date",
+        next_run_time=datetime.now() + timedelta(seconds=1)
+    )
 
 
 @Driver.on_bot_connect
 async def hide_window_main():
-    await NTQQ.hide_qq_window()  # 最小化QQ窗口
+    # Bot连接时最小化NTQQ窗口
+    await NTQQ.set_hwnd()  # 获取窗口句柄
+    NTQQ.wait_window()  # 等待窗口加载
+    await NTQQ.hide_window()
 
 
 @Driver.on_bot_disconnect
 async def restart_ntqq_main():
-    if PluginConfig.enable_restart_when_disc:  # 允许Bot断连的时候重启NTQQ
-        scheduler.add_job(  # 防一直处于on_bot_disconnect状态, 并防止shutdown的时候无法立即停止
-            restart_qq_main, "date",
-            next_run_time=datetime.now() + timedelta(seconds=1)
-        )
+    scheduler.add_job(  # 防一直处于on_bot_disconnect状态, 并防止shutdown的时候无法立即停止
+        restart_nqqq_main, "date",
+        next_run_time=datetime.now() + timedelta(seconds=1)
+    )
 
 
 @Driver.on_shutdown
 async def close_qq_main():
-    if PluginConfig.enable_close_qq_when_shutdown:  # 允许关闭nb时, 同时关闭NTQQ
-        NTQQ.close_qq_all_process()
+    NTQQ.set_shutdown()  # 设置shutdown状态
+    NTQQ.close()  # 关闭NTQQ进程
--- nonebot_plugin_ntqq_restart/config.py
+++ nonebot_plugin_ntqq_restart/config.py
@@ -4,13 +4,11 @@ from nonebot.config import Config as NB_Config
 
 
 class Config(NB_Config):
-    NTQQ_Path: Optional[str] = None  # NTQQ的.exe文件的路径
-    enable_login_when_nbrun: bool = False  # 允许插件加载时直接运行NTQQ并最小化窗口
-    enable_restart_when_disc: bool = True  # 允许Bot断连后重启
-    enable_close_qq_when_shutdown: bool = False  # 允许关闭nb时也关闭NTQQ
+    ntqq_path: Optional[str] = None  # NTQQ的.exe文件的路径
+    enable_restart: Optional[bool] = True  # 允许运行此插件
+    disable_restart_when_debug: Optional[bool] = False  # 日志等级为Debug时自动禁用插件
 
-    restart_after_disc_time: int = 10  # 在Bot断开连接{int}秒后开始重启
-    retry_max_time: Optional[int] = 5  # 重试窗口最小化最大时间(秒)
+    restart_time: int = 10  # 设置此项后, 将在断连{Restart_Time}(秒)后重启NTQQ
 
 
 __all__ = ["Config"]
